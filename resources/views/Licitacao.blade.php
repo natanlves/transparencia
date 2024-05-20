ea5d902afd9ce5fae70dac99644312b547cad1a6
Natan Alves <natan.alves.dev@gmail.com>
2024-05-20 10:56:35 -0400

    tests html with bootstrap

--- resources/views/Licitacao.blade.php
+++ resources/views/Licitacao.blade.php
@@ -4,13 +4,50 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="/css/styles.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
             <title>Transparencia</title>
         </head>        
         <body>
-
+            
             <h1>TITULO</h1>
             <p>aaaaaaa</p>
+            
+            
+            
+            <!--display heading-->
+            <h1 class="display-1">display 1</h1>
 
+            <!-- lead text aand assigment-->
+            <p class="lead">texto com lead</p>
+            <p class="lead text center">aaaaaaa</p>
+            <p class="lead text start">aaa</p>
+            <p class="lead text-end">aaaaa</p>   
+            <!-- text decoration and font weight-->
+            <p class="text-decoration-underline">this is underline text</p>
+            <p class="text-decoration-line-through">through text</p>
+            <p class="fw-bold">this is a bold text</p>      
+            
+            <!--text colours-->
+            <!--bg colours-->
+            <p class="text-white bg-primary">white text on primary bg</p>  
+            
+            <!--buttons-->
+            <button class="btn btn-primary">primary</button>
+            <!--anchor tags as button-->
+            <a href="#" class="btn btn-info">infoa anchor tag</a>
+            <!--button size-->
+            <button class="btn btn-lg btn-warning">large button</button>
+            <button class="btn btn-sm btn-sucess">small button</button>
+            <!-- button style-->            
+            <button class="btn btn-outline secondary">outline button secondary colour</button>
+            <!--button group-->
+            <div class="btn-group"></div>
+            
+            
+            
+            
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </body>
     </html>
 
